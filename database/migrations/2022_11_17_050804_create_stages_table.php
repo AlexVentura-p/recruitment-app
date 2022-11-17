f686d79b45d96a9a4c762fc732547603f0ccbfb1
AlexVentura-p <alex.ventura300@gmail.com>
2022-11-16 23:46:09 -0600

    Add applications and stages factory,model and migration

---
+++ database/migrations/2022_11_17_050804_create_stages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stages', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('company_id');
+            $table->unique(['name','company_id','stage_number']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stages');
+    }
+}
